f6f8e81ea0f15b1fe74b91909ae14972dcf09721
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-02-25 17:06:41 +0530

    Added request body

---
+++ Frontend/src/mocks/apiMocks.js
@@ -0,0 +1,39 @@
+// FOR CREATING ORDER
+// {
+//     "userId": "",
+//     "items": [
+//         {
+//             "productId": "63f0a80d48d02dd85d70e934",
+//             "quantity": 2,
+//             "price": 550
+//         },
+//         {
+//             "productId": "63f0a8d148d02dd85d70e936",
+//             "quantity": 1,
+//             "price": 119
+//         }
+//     ],
+//     "cartTotal": 1219,
+//     "discount": 0,
+//     "couponApplied": "",
+//     "address": "No.35, madiwala, Bengaluru.",
+//     "message": "Deliver between 5 to 5.30pm",
+//     "timeToDeliver": "Sat Feb 18 2023 16:33:03 GMT+0530 (India Standard Time)",
+//     "deliveryType": "door-delivery",
+//     "orderStatus": "order-received"
+// }
+
+// FOR CREATING FOOD
+// {
+//             "name": "BK Classis veg",
+//             "description": "Our tribute to classic american taste. BK veg patty, garden fresh crispy lettuce, juicy tomato(seasonal) & our signature sauce.",
+//             "category": "Burger",
+//             "cuisine": "American",
+//             "foodType": "veg",
+//             "price": 119,
+//             "tags": [],
+//             "discount": 0,
+//             "discountType": "",
+//             "sellerId": "",
+//             "namespace": "Cafe"
+//         }
